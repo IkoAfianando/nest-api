bf27148d8306be160234b0c5aa9bcf2a1db61e5b
IkoAfianando <ikoafianando123@gmail.com>
2022-10-02 20:27:14 +0700

    Update Controller Service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,19 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('iko')
+  getIko(): string {
+    return this.appService.getIko();
+  }
+
+  @Get('afianando')
+    getAfianando(): string {
+    const iko = () => {
+      const coba = [{name: 'iko', age: 20}, {name: 'afianando', age: 20}];
+      console.log(coba)
+      return coba;
+    }
+    return JSON.stringify(iko());
+  }
 }
